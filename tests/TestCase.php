726eccd28d8639ba5892ffe9d3b6287d01bc9960
gabrieladvent <bie.ritan112@gmail.com>
2026-05-28 13:36:16 +0700

    fix: error test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,9 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithoutVite; 
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use WithoutVite;
 }
